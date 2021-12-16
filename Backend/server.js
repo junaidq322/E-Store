18db5899d2e0c38fff457a86be5690abf60cd555
junaidq322 <junaidq322@gmail.com>
2021-12-16 18:39:24 +0500

    publish to heroku

--- Backend/server.js
+++ Backend/server.js
@@ -4,7 +4,7 @@ import orderRouter from "./routes/orderRouter.js";
 import productRouter from "./routes/productRouter.js";
 import uploadRouter from "./routes/uploadRouter.js";
 import userRouter from "./routes/userRouter.js";
-import path from 'path';
+import path from "path";
 
 const app = express();
 app.use(express.json());
@@ -14,17 +14,21 @@ mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/Estore", {
   useNewUrlParser: true,
 });
 
-app.use("/api/uploads",uploadRouter);
+app.use("/api/uploads", uploadRouter);
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
 app.use("/api/orders", orderRouter);
 
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
-app.get("/", (req, res) => {
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
+);
+/*app.get("/", (req, res) => {
   res.send("Server is ready");
 });
-
+*/
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
